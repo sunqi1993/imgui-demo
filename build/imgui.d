47254a358f819c1e9d30b09a49ad4c497c7f2da0
sunqi <156689004@qq.com>
2018-04-19 16:40:37 +0800

    1

---
+++ build/imgui.d
@@ -0,0 +1,34 @@
+
+{
+    ["program"] = "g++"
+,   ["objectfiles"] = 
+    {
+        "build/.objs/imgui/src/imgui/imgui_demo.cpp.o"
+    ,   "build/.objs/imgui/src/imgui/imgui_draw.cpp.o"
+    ,   "build/.objs/imgui/src/imgui/imgui.cpp.o"
+    ,   "build/.objs/imgui/src/imgui/imgui_impl_glfw_gl3.cpp.o"
+    }
+
+,   ["flags"] = 
+    {
+        "-L/usr/lib/x86_64-linux-gnu/"
+    ,   "-L/usr/local/lib"
+    ,   "-lglfw3"
+    ,   "-lGL"
+    ,   "-lXrandr"
+    ,   "-lX11"
+    ,   "-lXi"
+    ,   "-lXxf86vm"
+    ,   "-lpthread"
+    ,   "-ldl"
+    ,   "-lXinerama"
+    ,   "-lm"
+    ,   "-lrt"
+    ,   "-lXcursor"
+    ,   "-m64"
+    ,   "-L/usr/lib"
+    ,   "-shared"
+    ,   "-fPIC"
+    }
+
+}
